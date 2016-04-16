b2568b52dc8a591124477ac2294b3cf9ad4d2ac8
Ethan Davidson <theejdavidson@gmail.com>
2016-04-16 13:03:01 -0500

    week 12 hw 2 part b and c

--- week12_hw_1_and_2/Histogram.java
+++ week12_hw_1_and_2/Histogram.java
@@ -2,18 +2,32 @@ import java.util.*;
 public class Histogram {
     public static void main(String[] args) 
     {
-        Map<String, Integer> m = new HashMap<>();
-        // Initialize frequency table from command line
-        for (String a : args) 
-	{
-            Integer freq = m.get(a);
+        Map<Integer, Integer> m = new HashMap<>();
+		System.out.print("Enter integers for the Histogram when prompted ");
+		Scanner keyboard = new Scanner(System.in);
+		boolean done = false;
+	 while (!done)
+      {
+          System.out.println("Type an entry, type -1 to end:");
+          int entry = keyboard.nextInt( );
+		    if (entry == -1)
+			{
+              done = true;
+			}
+			else
+			{
+				 Integer freq = m.get(entry);
             if (freq == null) // means that word is new
-                m.put(a, 1); // if so, its count is 1, autoboxed to Integer
+                m.put(entry, 1); // if so, its count is 1, autoboxed to Integer
             else
-                m.put(a, freq + 1); // otherwise add 1 to its count
-        }
+                m.put(entry, freq + 1); // otherwise add 1 to its count;
+			}
+      }
 
-        System.out.println(m.size() + " distinct words:");
-        System.out.println(m); // Map overrides toString for a nice output
+        System.out.println(m.size() + " unique entries: ");
+		for(Map.Entry<Integer, Integer> pair : m.entrySet())
+		{
+        System.out.println(String.format("The number %d occurs %d times", pair.getKey(), pair.getValue())); // Map overrides toString for a nice output
+		}
     }
 }
