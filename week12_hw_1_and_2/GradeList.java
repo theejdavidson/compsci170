ee45bb0cdae3d86a629791e1a783d2e231c68890
Ethan Davidson <theejdavidson@gmail.com>
2016-04-16 11:26:37 -0500

    min max int

--- week12_hw_1_and_2/GradeList.java
+++ week12_hw_1_and_2/GradeList.java
@@ -9,20 +9,37 @@ public class GradeList
       System.out.println("Enter items for the list, when prompted.");
       boolean done = false;
       Scanner keyboard = new Scanner(System.in);
-
+	  int maxInt = 0;
+	  int minInt = Integer.MAX_VALUE;
       while (!done)
       {
           System.out.println("Type an entry, type -1 to end:");
           int entry = keyboard.nextInt( );
-		    if (entry != -1)
+		    if (entry == -1)
+			{
               done = true;
-          gradeList.add(entry);
+			}
+			else
+			{
+				if (minInt > entry)
+				{
+					minInt = entry;
+				}
+				if(maxInt < entry)
+				{
+					maxInt = entry;
+				}
+				gradeList.add(entry);
+			}
       }
 
       System.out.println("The list contains:");
       int listSize = gradeList.size( );
       for (int position = 0; position < listSize; position++)
           System.out.println(gradeList.get(position));
+	  
+	  		System.out.println("max int =" + maxInt);
+		System.out.println("min int =" + minInt);
 
 /* Alternate code for displaying the list
       System.out.println("The list contains:");
